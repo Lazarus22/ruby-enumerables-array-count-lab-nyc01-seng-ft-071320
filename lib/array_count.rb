ed33733a85b0497d2b1c255033ccd17485f3d457
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-05 18:50:42 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,7 +2,7 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |num|
     num > 0
-    new_array
+    new_array = []
     if array[index].string?
       new_array << array[index]
     end
