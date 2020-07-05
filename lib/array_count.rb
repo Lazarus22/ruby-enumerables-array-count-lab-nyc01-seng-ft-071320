8173d1f3b7920e1b51c88a6719d3fdc6533ae187
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-05 19:19:26 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
    array.count do |element|
-    element.is_a? String
+    element.is_a? string
   end
 end
 
@@ -9,6 +9,6 @@ def count_empty_strings(array)
   array.count do |element|
   #check if element is strings
   #check if element is empty
-  element.is_a? String && element.empty?
+  element.is_a? string && element.empty?
 end
 end
